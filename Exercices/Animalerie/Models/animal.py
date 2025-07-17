601866340f526f82dcad8efe907ac53237cdd7b0
hartjoa <hartjoa@gmail.com>
2025-07-17 10:22:25 +0200

    clean Animal class

--- Exercices/Animalerie/Models/animal.py
+++ Exercices/Animalerie/Models/animal.py
@@ -1,91 +1,34 @@
-class Animal:
-    __nom = ""
-    __poids = 0
-    __sexe = ""
-    __age = 0
-    __date_arrivee = ""
-    __probabilite_deces = 0
-    __espece = ""
+from abc import ABC, abstractmethod
 
-    # region properties
+class Animal(ABC):
+    """
+    Classe abstraite représentant un animal générique.
+    Ne peut pas être directement instanciée
+    """
 
-    # nom
-    @property
-    def nom(self):
-        return self.__nom
+    def __init__(self, nom, poids, taille, sexe, age, date_arrivee):
+        self.nom = nom
+        self.poids = poids
+        self.taille = taille
+        self.sexe = sexe
+        self.age = age
+        self.date_arrivee = date_arrivee
+        self.age_humain = self.calcul_age_humain()
+        self.vivant = True
     
-    @nom.setter
-    def nom(self, value):
-        self.__nom = value
-    
+    def calcul_age_humain(self):
+        return 7 * self.age
 
-    # poids
-    @property
-    def poids(self):
-        return self.__poids
-    
-    @poids.setter
-    def poids(self, value):
-        self.__poids = value
-
-    # sexe
-    @property
-    def sexe(self):
-        return self.__sexe
-    
-    @sexe.setter
-    def sexe(self, value):
-        self.__sexe = value
-
-    # age
-    @property
-    def age(self):
-        return self.__age
-    
-    @age.setter
-    def age(self, value):
-        self.__age = value
-
-    # date_arrivee
-    @property
-    def date_arrivee(self):
-        return self.__date_arrivee
-    
-    @date_arrivee.setter
-    def date_arrivee(self, value):
-        self.__date_arrivee = value
-    
-    # age_humain
-    @property
-    def age_humain(self):
-        return self.__age
-    
-    # probabilite_deces
-    @property
-    def probabilite_deces(self):
-        return self.__probabilite_deces
-    
-    # espece
-    @property
-    def espece(self):
-        return self.__espece
-    
-    # endregion
-
-    # region methods
-
-    def __init__(self, nom, sexe, age, date_arrivee):
-        self.__nom = nom
-        self.__sexe = sexe
-        self.__age = age
-        self.__date_arrivee = date_arrivee
-        
+    @abstractmethod
     def crier(self):
-        print(f"L'animal nommé '{self.nom}' pousse son cri")
+        """
+        Méthode abstraite: chaque animal doit définir son cri
+        """
+        pass                 # Prototype! Pas d'implémentation
 
-    def presenter(self):
-        print(
-            f"\n{self.nom} est un {self.__class__} {'male' if self.sexe == 'M' else 'femelle'} de {self.age} ans ({self.age_humain} ans humains)." + 
-            f"{'Il' if self.sexe == 'M' else 'Elle'} est à l'animalerie depuis le {self.date_arrivee}.")
-
-    # endregion
+    @abstractmethod
+    def risque_deces(self):
+        """
+        Méthode abstraite: chaque animal a son propre taux de mortalité
+        """
+        pass                 # Prototype! Pas d'implémentation
